5f437c864432d7344907b81f7845c9ba7bca32a3
Dante Dam <laggycomputer@yahoo.com>
2024-12-23 20:21:37 -0800

    better title

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -89,7 +89,7 @@ export default function QuizApp() {
 
     return (
         <div>
-            <h1>Quiz App</h1>
+            <h2>Anteater GPA Guesser</h2>
             {gameOver ? (
                 <div>
                     <form onSubmit={resetQuiz}>
