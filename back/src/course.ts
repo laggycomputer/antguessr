444aa0d5574229bff907727077e2ceb4227afebf
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-01-21 23:43:25 -0800

    Refactor backend stuff

---
+++ back/src/course.ts
@@ -0,0 +1,35 @@
+import { courses, years } from "./course-pool";
+import { GradeData, SavedOffering } from "./types";
+import { openAPIClient, shuffle, transformGPA } from "./util";
+
+export function getRandomizedOfferings() {
+    // offerings: [[["STATS", "67", 2022], undefined], [["STATS", "67", 2023], undefined], ...]
+    const offerings: SavedOffering[] = courses.flatMap(c => years.map(y => [[...c, y] as [string, string, string], undefined]))
+    return shuffle(offerings)
+}
+
+export async function getCourseGradeData(department: string, courseNumber: string, year: string): Promise<GradeData> {
+    const response = await openAPIClient.GET("/v2/rest/grades/aggregateByCourse", {
+        params: {
+            query: { department, courseNumber, year },
+        },
+    })
+    return response.data?.data[0]!
+}
+
+export async function getNextOfferingGPA(offerings: SavedOffering[]) {
+    let data, year
+    while (!data) {
+        const offering = offerings.pop() as SavedOffering
+        let [[department, courseNumber, offeredYear], existingData] = offering
+        year = offeredYear
+        data = existingData ?? await getCourseGradeData(department, courseNumber, year)
+
+        if (data && "averageGPA" in data) data.averageGPA = transformGPA(data.averageGPA as number, true)
+
+        offering[1] = data
+        offerings.unshift(offering)
+    }
+    return { data, year }
+}
+
--- back/src/util.ts
+++ back/src/util.ts
@@ -1,3 +1,13 @@
+import createClient from "openapi-fetch";
+import { SavedOffering } from "./types";
+import { paths } from "./anteaterapi";
+
+const headers = process.env.ANTEATER_API_TOKEN ? { Authorization: `Bearer ${process.env.ANTEATER_API_TOKEN}` } : undefined
+export const openAPIClient = createClient<paths>({
+    baseUrl: process.env.ANTEATER_API_ENDPOINT || "https://anteaterapi.com",
+    headers,
+})
+
 export function shuffle<T>(arr: T[]) {
     // (as discovered later) this is actually just `return arr.sort(() => Math.random() < 0.5 ? -1 : 1)`
 
@@ -29,4 +39,29 @@ export function transformGPA(gpa: number, correct = false) {
 
 export function digitSum(n: number) {
     return Array.from(Math.abs(n).toString().replace(".", "")).map(d => parseInt(d)).reduce((a, x) => a + x, 0)
+}
+
+export const gpaBuckets: [number, number][] = [
+    [0, 2],
+    [2, 2.5],
+    [2.5, 3],
+    [3, 3.25],
+    [3.25, 3.5],
+    [3.5, 4]
+]
+
+export function createOptionsFromGPA(data: Exclude<SavedOffering[1], undefined>) {
+    const average = data.averageGPA!
+    const buckets = gpaBuckets.slice()
+
+    const correctBucket = buckets.findIndex(([lower, upper]) => lower < average && average <= upper)
+    buckets.splice(correctBucket, 1)
+
+    const bucketToRandomGPA = ([lower, upper]: number[]) => transformGPA(randRange(lower!, upper!), false)
+    const initialOptions = [
+        ...buckets.map(bucketToRandomGPA),
+        data.averageGPA // previously transformed
+    ]
+
+    return [...new Set(initialOptions)].toSorted()
 }
